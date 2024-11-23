9530512f947d6d63b3188918d2a11042668ec089
Sundar2k4 <csundar2004@gmail.com>
2024-11-23 10:58:03 +0530

    vercel

--- api/Index.js
+++ api/Index.js
@@ -23,7 +23,7 @@ app.use('/uploads',express.static(__dirname+'/uploads')) // to specify the whole
 const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://deploy-mern-1whq.vercel.app'
+    'https://grand-mark-hotel-reservator-client.vercel.app/'
 ];
 
 app.use(cors({
