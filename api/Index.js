284680a9d82ebae4e32563de0585911822ac62d3
Sundar.c <csundar2004@gmail.com>
2025-04-22 19:23:44 +0530

    deploy

--- api/Index.js
+++ api/Index.js
@@ -270,5 +270,12 @@ app.get('/bookings', async (req,res)=>
    
 })
 
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
